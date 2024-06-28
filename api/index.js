bf5c4999af517cdb0517494ba93aa325dc6d5617
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2024-06-29 00:52:30 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 
+ import dotenv from "dotenv" ;
+ dotenv.config();
 
-mongoose.connect
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
 const app = express();
 
-
-app.listen(3000,()=>{
- console.log("server is listenign ")  
-})
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
