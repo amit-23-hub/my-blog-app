4e72c2b58e634de3456c9c1ec958016965a03dcc
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2024-06-28 23:31:25 +0530

    header section created

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import mongoose from "mongoose";
+
+
+mongoose.connect
+const app = express();
+
+
+app.listen(3000,()=>{
+ console.log("server is listenign ")  
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import 'flowbite/dist/flowbite.css';
+
 import { BrowserRouter, Route ,Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Dashaboard from './pages/Dashaboard'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
+import About from './pages/about'
+import Header from './components/header'
 
 export default function App() {
   return (
     <BrowserRouter> 
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/about" element={<About/>}></Route>
